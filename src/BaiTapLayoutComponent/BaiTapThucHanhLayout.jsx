79d5cbbcf05e055c861aa65db8d980e6048f132c
quangman <quangman@mail.com>
2023-11-07 01:09:11 +0700

    baitap1

---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Item from "./Item";
+import Footer from "./Footer";
+import Banner from "./Banner";
+
+export default function BaiTapThucHanhLayout() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Item />
+      <Footer />
+    </>
+  );
+}
