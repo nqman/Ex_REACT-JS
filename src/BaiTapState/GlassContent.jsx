a63d5264ab8de8bb94e3367bd17fcaa88510e049
quangman <quangman@mail.com>
2023-11-11 13:27:19 +0700

    updatebt2

--- src/BaiTapState/GlassContent.jsx
+++ src/BaiTapState/GlassContent.jsx
@@ -32,28 +32,28 @@ export default function GlassContent() {
           <img src="./img/v1.png" alt="v1" />
         </button>
         <button onClick={() => handleChangeGlass(2)}>
-          <img src="./img/v2.png" alt="v1" />
+          <img src="./img/v2.png" alt="v2" />
         </button>
         <button onClick={() => handleChangeGlass(3)}>
-          <img src="./img/v3.png" alt="v1" />
+          <img src="./img/v3.png" alt="v3" />
         </button>
         <button onClick={() => handleChangeGlass(4)}>
-          <img src="./img/v4.png" alt="v1" />
+          <img src="./img/v4.png" alt="v4" />
         </button>
         <button onClick={() => handleChangeGlass(5)}>
-          <img src="./img/v5.png" alt="v1" />
+          <img src="./img/v5.png" alt="v5" />
         </button>
         <button onClick={() => handleChangeGlass(6)}>
-          <img src="./img/v6.png" alt="v1" />
+          <img src="./img/v6.png" alt="v6" />
         </button>
         <button onClick={() => handleChangeGlass(7)}>
-          <img src="./img/v7.png" alt="v1" />
+          <img src="./img/v7.png" alt="v7" />
         </button>
         <button onClick={() => handleChangeGlass(8)}>
-          <img src="./img/v8.png" alt="v1" />
+          <img src="./img/v8.png" alt="v8" />
         </button>
         <button onClick={() => handleChangeGlass(9)}>
-          <img src="./img/v9.png" alt="v1" />
+          <img src="./img/v9.png" alt="v9" />
         </button>
       </div>
     </div>
